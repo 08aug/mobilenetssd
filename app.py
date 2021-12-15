53b0b98d6aa652583a7067266bf9ca0b3d488090
08aug <89176609+08aug@users.noreply.github.com>
2021-12-15 15:07:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -161,18 +161,12 @@ def event_handle(event,json_line):
             response = response.json()
             replyObj = TextSendMessage(text=str(response))
             line_bot_api.reply_message(rtoken, replyObj)
-        elif msg == "covid trat" :
-            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces"
-            response = requests.get(url)
-            response = response.json()
-            replyObj = TextSendMessage(text=str(response))
-            line_bot_api.reply_message(rtoken, replyObj)
         else :
-           headers = request.headers
-           json_headers = ({k:v for k, v in headers.items()})
-           json_headers.update({'Host':'bots.dialogflow.com'})
-           url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/b12b1dc7-ee3f-4dd0-a31f-d25690e6fc42"
-           requests.post(url,data=json_line, headers=json_headers)
+            headers = request.headers
+            json_headers = ({k:v for k, v in headers.items()})
+            json_headers.update({'Host':'bots.dialogflow.com'})
+            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/b12b1dc7-ee3f-4dd0-a31f-d25690e6fc42"
+            requests.post(url,data=json_line, headers=json_headers)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
